91c11506a94d5151d5c507d6ff1fbfd9e5cdf0e4
Sigarya <79363716+Sigarya@users.noreply.github.com>
2025-05-13 15:27:47 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -25,7 +24,8 @@ const App = () => (
           <GameProvider>
             <Toaster />
             <Sonner />
-            <BrowserRouter>
+            {/* הוספת basename ל-BrowserRouter */}
+            <BrowserRouter basename="/semantle-plus">
               <Routes>
                 <Route path="/" element={<Index />} />
                 <Route path="/about" element={<About />} />
