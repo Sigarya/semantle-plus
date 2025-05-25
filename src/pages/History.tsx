4957969cc40132dc7f1450d8a1116ba92345fa08
Sigarya <79363716+Sigarya@users.noreply.github.com>
2025-05-25 16:44:32 +0300

    Update History.tsx

--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -8,8 +8,8 @@ const History = () => {
       <div className="max-w-3xl mx-auto mb-6 p-4 bg-primary-50 dark:bg-slate-800 rounded-md border border-primary-200 dark:border-slate-700 text-center">
         <p className="text-primary-800 dark:text-primary-300">
           רוצים לנסות שוב? פספסתם יום? כאן תוכלו לשחק בגרסאות קודמות של המשחק היומי. 
-          המילים שעד 8 במאי 2025 לקוחות מתוך המשחק המקורי. 
-          החל מ-9 במאי, בכל יום מחכה לכם מילה חדשה לגמרי.
+          המילים עד 24 במאי 2025 לקוחות מתוך המשחק המקורי. 
+          החל מ-25 במאי, בכל יום מחכה לכם מילה חדשה לגמרי.
         </p>
       </div>
       <HistoryPanel />
