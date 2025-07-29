205cac978c8d9d4bfc9653857d5f6c155684cdea
Sigarya <79363716+Sigarya@users.noreply.github.com>
2025-07-30 02:26:27 +0300

    Update History.tsx

--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -7,7 +7,7 @@ const History = () => {
     <PageLayout title="היסטורית משחקים">
       <div className="max-w-3xl mx-auto mb-6 p-4 bg-primary-50 dark:bg-slate-800 rounded-md border border-primary-200 dark:border-slate-700 text-center">
         <p className="text-primary-800 dark:text-primary-300">
-          רוצים לנסות שוב? פספסתם את הסמנטעל אתמול? כאן תוכלו לשחק בגרסאות קודמות של המשחק היומי. <br>
+          רוצים לנסות שוב? פספסתם את הסמנטעל אתמול? כאן תוכלו לשחק בגרסאות קודמות של המשחק היומי.<br />
         בכל יום קצת אחרי השעה 3:00 יעלה המשחק של אתמול ויוצג כאן ובדף הראשי.
         </p>
       </div>
