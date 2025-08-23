1b7935da6940b5b8c4c26268c761b07314ac4bfb
Sigarya <79363716+Sigarya@users.noreply.github.com>
2025-08-23 17:41:42 +0300

    Update GameBoard.tsx

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -324,7 +324,7 @@ const GameBoard = React.memo(() => {
                 <path d="M22 21v-2a4 4 0 0 0-3-3.87"/>
                 <path d="M16 3.13a4 4 0 0 1 0 7.75"/>
               </svg>
-              שחק עם חברים
+              שחק עם חברים (ניסיוני)
             </Button>
           </Link>
         </div>
